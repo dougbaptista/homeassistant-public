ec4bf47a1c43324ef4a271e9376556c2989d6fd8
Douglas Baptista <doug.baptista@gmail.com>
2019-07-14 12:10:32 -0300

    Add files via upload

---
+++ styles.js
@@ -0,0 +1,12 @@
+var cssId = 'myCss';  // you could encode the css path itself to generate id..
+if (!document.getElementById(cssId))
+{
+    var head  = document.getElementsByTagName('head')[0];
+    var link  = document.createElement('link');
+    link.id   = cssId;
+    link.rel  = 'stylesheet';
+    link.type = 'text/css';
+    link.href = '/local/stylesheet.css';
+    link.media = 'all';
+    head.appendChild(link);
+}
